9bc22632d9570fe4e73c40189db010d1bb603dbd
Ali <alikdg779@gmail.com>
2026-05-08 20:59:43 +0300

    Create Movie.cs

---
+++ Model/Movie.cs
@@ -0,0 +1,11 @@
+namespace FilmListesi_proje1.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string MovieName { get; set; }
+
+        public string Genre { get; set; }
+    }
+}
